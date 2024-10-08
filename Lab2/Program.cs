0690d7ca9cc8703b4f0d677ac2b7b06d9e402b96
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-08 19:39:57 +0700

    a

---
+++ Lab2/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myInt = {1,2,3,4,5,6,7,8,9};
+        int sum = 0;
+        for(var i = 0; i < myInt.Length;i++){
+            sum += myInt[i];
+        }
+        Console.WriteLine($"Tong: {sum}" );
+        
+    }
+}
